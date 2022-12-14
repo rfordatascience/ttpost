e60bd84865de4472af68bb7578343f79b997ac34
Jon Harmon <jonthegeek@gmail.com>
2022-12-14 15:29:08 -0600

    Move the tweeting back to a separate script.

---
+++ tweet.R
@@ -0,0 +1,13 @@
+token <- rtweet::rtweet_bot(
+  api_key = Sys.getenv("TTBOT_API_KEY"),
+  api_secret = Sys.getenv("TTBOT_API_SECRET"),
+  access_token = Sys.getenv("TTBOT_ACCESS_TOKEN"),
+  access_secret = Sys.getenv("TTBOT_ACCESS_TOKEN_SECRET")
+)
+tweet_start_ts <- Sys.time()
+rtweet::post_tweet(
+  status = glue::glue(
+    "This is another test tweet, sent at {tweet_start_ts}."
+  ),
+  token = token
+)
