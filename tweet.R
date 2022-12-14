5e018805969860b0c109efa20ff177c07492982b
Jon Harmon <jonthegeek@gmail.com>
2022-12-14 14:38:10 -0600

    A simple tweet script.

---
+++ tweet.R
@@ -0,0 +1,7 @@
+rtweet::rtweet_bot(
+  api_key = Sys.getenv("TTBOT_API_KEY"),
+  api_secret = Sys.getenv("TTBOT_API_SECRET"),
+  access_token = Sys.getenv("TTBOT_ACCESS_TOKEN"),
+  access_secret = Sys.getenv("TTBOT_ACCESS_TOKEN_SECRET")
+)
+rtweet::post_tweet(status = "This is another test tweet, sent using GHA!")
