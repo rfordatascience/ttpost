b1d11ad186381eb47d8e5883ae5fe4828889b3da
Jon Harmon <jonthegeek@gmail.com>
2022-12-15 12:09:02 -0600

    Make an actual working process.
    I *think* this will be fine, but I might have to explicitly save the images as temp files or something (but in theory the runner will destroy itself). I'll need to see if it caches the images, though.

--- tweet.R
+++ tweet.R
@@ -1,25 +1,16 @@
 source("helpers.R")
 
-token <- rtweet::rtweet_bot(
-  api_key = Sys.getenv("TTBOT_API_KEY"),
-  api_secret = Sys.getenv("TTBOT_API_SECRET"),
-  access_token = Sys.getenv("TTBOT_ACCESS_TOKEN"),
-  access_secret = Sys.getenv("TTBOT_ACCESS_TOKEN_SECRET")
-)
-
 next_tt_num <- next_week_num()
 next_tt_year <- next_year()
 
-available_datasets <- tidytuesdayR::tt_datasets(next_tt_year)
+available_datasets <- tidytuesdayR::tt_datasets(next_tt_year) |> 
+  unclass() |> 
+  tibble::as_tibble()
 
 if (next_tt_num %in% available_datasets$Week) {
-  tweet_start_ts <- Sys.time()
-  rtweet::post_tweet(
-    status = glue::glue(
-      "This is another test tweet, sent at {tweet_start_ts}."
-    ),
-    token = token
-  )
+  data_title <- available_datasets$Data[available_datasets$Week == next_tt_num]
+  
+  tt_tweet(data_title)
 } else {
   stop("No data available!")
 }
