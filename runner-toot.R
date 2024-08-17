3e234bb0f60696d01f30f524419b8e56e1c4e8a2
Jon Harmon <jonthegeek@gmail.com>
2024-08-17 13:05:47 -0700

    Submission drive (#52)
    * Call for help
    
    And give credit on mastodon.
    
    * Update pkg versions.

--- runner-toot.R
+++ runner-toot.R
@@ -3,4 +3,14 @@ source("runner-shared.R", local = TRUE)
 
 # Toot.
 source("helpers-mastodon.R", local = TRUE)
+
+if (length(metadata$credit$mastodon)) {
+  credit <- glue::glue(
+    "Curator: {metadata$credit$mastodon}"
+  )
+  if (length(credit) && (nchar(status_msg) + nchar(credit) < 500)) {
+    status_msg <- paste(credit, status_msg, sep = "\n")
+  }
+}
+
 tt_toot(status_msg, img_paths, alt_text)
