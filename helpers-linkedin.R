ecd9b49776352dfa2cc5895c3296e610c18f31e1
Jon Harmon <jonthegeek@gmail.com>
2025-02-03 08:40:14 -0600

    Only roll LI back 1 month

--- helpers-linkedin.R
+++ helpers-linkedin.R
@@ -16,7 +16,7 @@ li_client <- httr2::oauth_client(
 
 # Automatically set the LinkedIn API version to 2 months ago since they
 # constantly change that without changing core features.
-li_version_date <- lubridate::rollback(lubridate::rollback(lubridate::today()))
+li_version_date <- lubridate::rollback(lubridate::today())
 
 li_version <- paste0(
   lubridate::year(li_version_date), 
