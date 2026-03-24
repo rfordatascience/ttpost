9b11e7dbf383891703089a28b5c149ef38a71ecd
Jon Harmon <jonthegeek@gmail.com>
2026-03-24 09:33:36 -0500

    fix: reskeet stability and action versions

--- dslc-reskeet.R
+++ dslc-reskeet.R
@@ -12,9 +12,9 @@ post_uri <- bsky_result$uri
 
 bskyr::set_bluesky_user("dslc.io")
 bskyr::set_bluesky_pass(Sys.getenv("BSKY_DSLC"))
-bskyr::bs_auth(bskyr::get_bluesky_user(), bskyr::get_bluesky_pass(), NULL)
+auth <- bskyr::bs_auth(bskyr::get_bluesky_user(), bskyr::get_bluesky_pass(), NULL)
 
-bskyr::bs_like(post_uri)
+bskyr::bs_like(post_uri, auth = auth)
 
 reskeet_msg <- paste(
   "It's #TidyTuesday y'all! Show us what you made on our Slack at https://dslc.io!",
@@ -24,5 +24,28 @@ reskeet_msg <- paste(
 
 result <- bskyr::bs_post(
   reskeet_msg,
-  quote = post_uri
+  quote = post_uri,
+  embed = FALSE,
+  auth = auth
 )
+
+# Pin the reskeet to the DSLC profile
+profile <- bskyr::bs_get_record(
+  repo = auth$did,
+  collection = "app.bsky.actor.profile",
+  rkey = "self",
+  auth = auth,
+  clean = FALSE
+)
+
+profile$value$pinnedPost <- list(uri = result$uri, cid = result$cid)
+
+httr2::request("https://bsky.social/xrpc/com.atproto.repo.putRecord") |>
+  httr2::req_auth_bearer_token(auth$accessJwt) |>
+  httr2::req_body_json(list(
+    repo       = auth$did,
+    collection = "app.bsky.actor.profile",
+    rkey       = "self",
+    record     = profile$value
+  )) |>
+  httr2::req_perform()
